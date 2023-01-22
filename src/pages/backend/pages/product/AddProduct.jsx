6794128a38ece53f135dece7e600f9ca7952c962
Sohel51 <sohelcutyy@gmail.com>
2023-01-22 15:34:07 +0600

    Create Product Set

--- src/pages/backend/pages/product/AddProduct.jsx
+++ src/pages/backend/pages/product/AddProduct.jsx
@@ -1,8 +1,174 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { Editor } from '@tinymce/tinymce-react';
 
 const AddProduct = () => {
+  const [formErrors, setFormErrors] = useState();
+  const [categories, setCategories] = useState([]);
+  const editorRef = useRef(null);
+  const previewImage = useRef(null);
+
+  useEffect(() => {
+    getCategory();
+  }, [])
+
+  const getCategory = () => {
+    fetch('http://localhost:5000/category/all', {
+      method: "GET",
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token')
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+        setCategories(res);
+      })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setFormErrors({}); // for remove the errors
+    let formData = new FormData(e.target);
+    formData.append('description', editorRef.current.getContent());
+    fetch(`http://localhost:5000/product/create`, {
+      method: "POST",
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token')
+      },
+      body: formData,
+    })
+      .then(async (res) => {
+        let data = await res.json();
+        return {
+          status: res.status,
+          data,
+        }
+      })
+      .then(res => {
+        console.log(res);
+        if (res.status === 422) {
+          let tempError = {
+            title: [],
+            category: [],
+            price: [],
+            image: [],
+            description: [],
+          };
+          res.data.errors.forEach((e, index) => {
+            tempError[e.param].push(
+              <li key={index} className='text-danger'>
+                {e.msg}
+              </li>
+            )
+          });
+          setFormErrors(tempError)
+        }
+
+        // reset the form
+        if (res.status === 201) {
+          e.target.reset();
+          window.alert('New Product Created')
+        }
+      })
+  }
+
   return (
-    <div>AddProduct</div>
+    <div className="d-card">
+      <div className="card-header m-0 align-items-center d-flex flex-wrap justify-content-between">
+        <h3>Add Product</h3>
+        <div>
+          <Link to="/admin/listproduct" className='btn btn-md-size btn-info'><i className='fas fa-minus'>Product List</i></Link>
+        </div>
+      </div>
+      <div className="card-body">
+        <form action="" encType='multipart/form-data' onSubmit={submitHandler}>
+          <div className="from-group mb-3">
+            <label htmlFor="">Title</label>
+            <input type="text" className='form-control' name='title' />
+            <ul>
+              {formErrors?.title}
+            </ul>
+          </div>
+          <div className="from-group mb-3">
+            <label htmlFor="">Category</label>
+            <select className='form-control' name="category" id="">
+              {
+                categories.map(i => {
+                  return <option key={i._id} value={i._id}>{i.title}</option>
+                })
+              }
+            </select>
+            <ul>
+              {formErrors?.category}
+            </ul>
+          </div>
+          <div className="from-group mb-3">
+            <label htmlFor="">Price</label>
+            <input type="text" className='form-control' name='price' />
+            <ul>
+              {formErrors?.price}
+            </ul>
+          </div>
+          <div className="from-group mb-3">
+            <label htmlFor="">Discount</label>
+            <input type="text" className='form-control' name='discount' />
+            <ul>
+              {formErrors?.discount}
+            </ul>
+          </div>
+          <div className="from-group mb-3">
+            <label htmlFor="">Discount Price</label>
+            <input type="text" className='form-control' name='discountPrice' />
+            <ul>
+              {formErrors?.discountPrice}
+            </ul>
+          </div>
+          <div className="from-group mb-3">
+            <label htmlFor="">Discount Date</label>
+            <input type="date" className='form-control' name='discountDate' />
+            <ul>
+              {formErrors?.discountDate}
+            </ul>
+          </div>
+          <div className="from-group mb-3">
+            <label htmlFor="">Image</label>
+            <input onChange={(e) => previewImage.current.src=window.URL.createObjectURL(e.target.files[0])}
+              type="file"
+              className='' name='image' />
+            <img src="" style={{width: 120, marginTop: 5,}} ref={previewImage} />
+            <ul>
+              {formErrors?.image}
+            </ul>
+          </div>
+          <div className="from-group mb-3">
+            <label htmlFor="">Description</label>
+            <Editor
+              onInit={(evt, editor) => editorRef.current = editor}
+              initialValue="<p>This is the initial content of the editor.</p>"
+              init={{
+                height: 500,
+                menubar: false,
+                plugins: [
+                  'advlist autolink lists link image charmap print preview anchor',
+                  'searchreplace visualblocks code fullscreen',
+                  'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar: 'undo redo | formatselect | ' +
+                  'bold italic backcolor | alignleft aligncenter ' +
+                  'alignright alignjustify | bullist numlist outdent indent | ' +
+                  'removeformat | help',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+              }}
+            />
+            {/* <textarea type="text" className='form-control h-100' rows="10" name='description' /> */}
+            <ul>
+              {formErrors?.description}
+            </ul>
+          </div>
+          <button className='btn btn-primary'>Submit</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/pages/backend/pages/product/ListProduct.jsx
+++ src/pages/backend/pages/product/ListProduct.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ListProduct = () => {
   return (
-    <div>ListProduct</div>
+    <div className="d-card">
+      <div className="card-header m-0 align-items-center d-flex flex-wrap justify-content-between">
+        <h3>All Products</h3>
+        <div>
+          <Link to="/admin/addproduct" className='btn btn-md-size btn-info'><i className='fas fa-plus'>Add Product</i></Link>
+        </div>
+      </div>
+      <div className="card-body table-responsive">
+        <table className='table table-hover text-center'>
+          <thead>
+            <tr>
+              <th>SI</th>
+              <th>Title</th>
+              <th>Image</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Discount</th>
+              <th>D Price</th>
+              <th>Creator</th>
+              <th style={{ width: '150px' }}>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <div className='d-flex gap-2 flex-wrap justify-content-end' style={{ gap: '5px' }}>
+                  <a href="" className='btn btn-sm btn-info'>Details</a>
+                  <Link to="" className='btn btn-sm btn-warning'>Edit</Link>
+                  <a href="" className='btn btn-sm btn-danger'>Delete</a>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
